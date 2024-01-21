e3fe3cfda194e56f66c83192a65cb9f0d6c008f7
ShaikhAdnan7621 <119574362+ShaikhAdnan7621@users.noreply.github.com>
2024-01-21 18:46:59 +0530

    route.js

--- app/api/admin/products/image/add/route.js
+++ app/api/admin/products/image/add/route.js
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
 import { writeFile, mkdir } from "fs/promises";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 export async function POST(req, resp) {
     try {
@@ -19,11 +21,14 @@ export async function POST(req, resp) {
         const datePath = `${today.getFullYear()}-${String(
             today.getMonth() + 1,
         ).padStart(2, "0")}-${String(today.getDate()).padStart(2, "0")}`;
-        const folderPath = `./public/Productimage/${datePath}`;
+        const __dirname = dirname(fileURLToPath(import.meta.url));
+        const folderPath = `${__dirname}/public/Productimage/${datePath}`;
         await mkdir(folderPath, { recursive: true });
         const location = `/Productimage/${datePath}/${file.name}`;
         const path = `${folderPath}/${file.name}`;
         await writeFile(path, buffer);
+
+        console.log(`File saved at: ${path}`); // This will log the path of the file
 
         return NextResponse.json({
             message: "File saved successfully",
