746d3b988f3c842453d808b53c83dc965fed26ac
ShaikhAdnan7621 <119574362+ShaikhAdnan7621@users.noreply.github.com>
2024-01-21 10:06:12 +0530

    Delete app directory

--- app/Restricted/page.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Page() {
-    return (
-        <div className="text-center dark:text-white">
-            you not have this page permission
-        </div>
-    );
-}
--- app/page.js
+++
@@ -1,9 +0,0 @@
-import Homepage from "@/components/homepage/Homepage";
-export default function Home() {
-    
-    return (
-        <main className="text-black  dark:text-white">
-            <Homepage />
-        </main>
-    );
-}
